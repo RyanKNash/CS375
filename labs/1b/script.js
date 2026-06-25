13535cf02617aab4e49198fdc6b824d609c75a57
RKNash <145068529+RyanKNash@users.noreply.github.com>
2026-06-25 15:04:12 -0400

    ...

--- labs/1b/script.js
+++ labs/1b/script.js
@@ -1,14 +1,15 @@
 //Q1
 function sumDiagonals(arr, row, col) {
     let sum = 0;
-    let diagonals = [];
 
-    if (row >= 0 && row < arr.length && col >= 0 && 
-        col < arr[0].length) { 
-        continue;
-        } else {
-        return(console.log("Out of bounds"));
+    if (
+        row < 0 || row >= arr.length ||
+        col < 0 || col >= arr[0].length
+    ) {
+        console.log("Out of bounds");
+        return;
     }
+
     let diagonals = [
         [row - 1, col - 1],
         [row - 1, col + 1],
@@ -16,18 +17,20 @@ function sumDiagonals(arr, row, col) {
         [row + 1, col + 1],
     ];
 
-
     for (let [r, c] of diagonals) {
-        if (r >= 0 && r < arr.length && c >= 0 && c < arr[0].length) {
-        sum += arr[r][c];
+        if (
+            r >= 0 && r < arr.length &&
+            c >= 0 && c < arr[0].length
+        ) {
+            sum += arr[r][c];
         }
     }
 
     return sum;
 }
 
-let arr = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]
-sumDiagonals(arr, 4, 2)
+//let arr = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]
+//sumDiagonals(arr, 4, 2)
 //sumDiagonals(arr, 0, 0)
 
 //Q2
