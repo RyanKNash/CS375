4ff20d8cff38ee780fac84e9d3f571fdbd84047b
RKNash <145068529+RyanKNash@users.noreply.github.com>
2026-06-25 14:43:05 -0400

    l2bq3

--- labs/1b/script.js
+++ labs/1b/script.js
@@ -2,12 +2,16 @@
 function sumDiagonals(arr, row, col) {
     let sum = 0;
 
-    let diagonals = [
-        [row - 1, col - 1],
-        [row - 1, col + 1],
-        [row + 1, col - 1],
-        [row + 1, col + 1],
-    ];
+    if (row <= 0 || row > arr.length || col <= 0 || col > arr[0].length) {
+        let diagonals = [
+            [row - 1, col - 1],
+            [row - 1, col + 1],
+            [row + 1, col - 1],
+            [row + 1, col + 1],
+        ];
+    } else {
+        return(console.log("Out of bounds"));
+    }
 
     for (let [r, c] of diagonals) {
         if (r >= 0 && r < arr.length && c >= 0 && c < arr[0].length) {
@@ -18,8 +22,8 @@ function sumDiagonals(arr, row, col) {
     return sum;
 }
 
-//let arr = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]
-//sumDiagonals(arr, 1, 2)
+let arr = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]
+sumDiagonals(arr, 4, 2)
 //sumDiagonals(arr, 0, 0)
 
 //Q2
@@ -36,4 +40,37 @@ function copyInc(arr) {
     return result;
 }
 
-console.log(copyInc([[1,2,3],[4,5,6],[7,8,9]]));
+//console.log(copyInc([[1,2,3],[4,5,6],[7,8,9]]));
+
+//Q3
+let obj = {
+    999: null,
+    a: 1,
+    b: {
+        xyz: "abc",
+        foobar: {
+            hello_world: [1, 2, 3],
+        },
+    },
+    c: "def",
+};
+
+function isLiteralObj(x) {
+    return (!!x) && (x.constructor === Object);
+}
+
+function deepObjPrint(obj, indent = 0) {
+    let spaces = " ".repeat(indent);
+
+    for (let [key, value] of Object.entries(obj)) {
+        if (isLiteralObj(value)) {
+            console.log(`${spaces}${key}: {`);
+            deepObjPrint(value, indent + 2);
+            console.log(`${spaces}}`);
+        } else {
+            console.log(`${spaces}${key}: ${value}`);
+        }
+    }
+}
+
+deepObjPrint(obj);
