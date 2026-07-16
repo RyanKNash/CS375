edc9bf01bcf5d853537fdcaf3cd46b1235c2aa4d
RKNash <145068529+RyanKNash@users.noreply.github.com>
2026-07-16 12:33:07 -0400

    ...

---
+++ homeworks/4/starter/app/server.js
@@ -0,0 +1,46 @@
+let axios = require("axios");
+let express = require("express");
+let app = express();
+let apiFile = require("../env.json");
+let apiKey = apiFile["api_key"]; // use this to make requests
+let baseUrl = apiFile["api_url"]; // use this to make requests
+let port = 3000;
+let hostname = "localhost";
+app.use(express.static("public"));
+// don't change code above this line
+
+app.get("/feels-like", async (req, res) => {
+  let zip = req.query.zip;
+
+  if (!zip) {
+    return res.status(400).json({ error: "Please enter a zip code" });
+  }
+
+  try {
+    let response = await axios.get(baseUrl, {
+      params: { zip: `${zip},us`, appid: apiKey },
+    });
+    let feelsLikeKelvin = response.data.main.feels_like;
+    let feelsLikeFahrenheit = ((feelsLikeKelvin - 273.15) * 9) / 5 + 32;
+
+    res.json({
+      zip: zip,
+      "feels-like-fahrenheit": Math.round(feelsLikeFahrenheit),
+    });
+  } catch (error) {
+    console.error(error.message);
+
+    if (error.response) {
+      let apiMessage = error.response.data && error.response.data.message;
+      return res.status(400).json({
+        error: apiMessage || "Could not retrieve weather data",
+      });
+    }
+
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log(`http://${hostname}:${port}`);
+});
